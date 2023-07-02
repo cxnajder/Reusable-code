546361dde3798bc26eeb604ff8a68896924c1194
cxnajder <cxnajder@github.com>
2023-07-02 11:55:14 +0200

    Copy Constructor - memory-multiple-release error fix

--- pointers/buffer.cpp
+++ pointers/buffer.cpp
@@ -6,15 +6,17 @@ class Buffer{
     int size;
 public:
     explicit Buffer(int bufferSize);
+    Buffer(const Buffer & origin);  // COPY CONSTRUCTOR
     ~Buffer();
 };
 
-Buffer::Buffer(int bufferSize): size(bufferSize){
+Buffer::Buffer(int bufferSize){
 
     if (bufferSize < 1)
         throw std::runtime_error("Invalid buffer size.");
 
-    data = new int[size];
+    data = new int[bufferSize];
+    size = bufferSize;
 
     // MALLOC vs NEW difference:
 
@@ -23,29 +25,25 @@ Buffer::Buffer(int bufferSize): size(bufferSize){
     // malloc requireS to specify number of bytes
 }
 
+Buffer::Buffer(const Buffer & origin){ // COPY CONSTRUCTOR
+    // REQUIRED TO PREVENT THE SAME MEMORY FROM BEAING RELEASED MULTIPLE TIMES!!!
+    data = new int[origin.size];
+    size = origin.size;
+    std::copy(origin.data, origin.data + origin.size, data);
+}
+
 Buffer::~Buffer() {
     delete [] data; 
     // REQUIRED TO FREE MEMORY IN DESTRUCTOR TO PREVENT MEMORY LEAK!!!
 }
 
-void buffor_error(Buffer b){
+void buffer_test(Buffer b){
     return;
 }
 
 int main(int argc, char const *argv[])
 {
     Buffer b1(4);
-    buffor_error(b1); // ERROR: free(): double free detected in tcache 2
-
-    // b1 HAS BEEN COPIED TO buffor_error FUNCTION WITH POINTER TO THE SAME MEMORY
-    // (now we have 2 buffers - one in main and one in function - BOTH POINTING TO THE SAME MEMORY!)
-    // WHEN THE FUNCTION ENDED, THE OBJECT (copy of b1) WAS DESTROYED
-    // THAT CALLED THE DESTRUCTOR OF THE OBJECT WITCH FREED THE MEMORY OF b1
-    // b1 STILL EGZISTS BUT ITS POINTING TO THE MEMORY THAT HAS BEEN FREED!!!
-
-    // AFTER EXITING MAIN b1 WILL TRY TO RELEASE MEMORY THAT HAS BEEN ALREADY RELEASED!
-
-    // TO SOLVE THIS A NEW COPU CONSTRUCTOR IS REQUIRED!
-
+    buffer_test(b1); // no error
     return 0;
 }
