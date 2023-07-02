155f06c453346799978ff8365f2862f4730ddca9
cxnajder <cxnajder@github.com>
2023-07-02 14:07:29 +0200

    rule of 3 and 0 in comment; RVO examples

--- pointers/buffer.cpp
+++ pointers/buffer.cpp
@@ -92,6 +92,21 @@ void buffer_test(Buffer b){
     return;
 }
 
+Buffer RVO_test(){
+    Buffer tmp(5);
+    return tmp;
+}
+// RVO - Return Value Optimalization
+
+Buffer RVO_test2(){
+    Buffer tmp1(2);
+    Buffer tmp2(3);
+    
+    if (rand() % 2)
+        return tmp1;
+    return tmp2;
+}
+
 int main(int argc, char const *argv[])
 {
     Buffer b1(4);
@@ -101,5 +116,24 @@ int main(int argc, char const *argv[])
     Buffer b3(2);
     b2 = b1 = b3;   // no error
 
+    Buffer b4 = RVO_test(); // RVO - Return Value Optimalization / copy elision
+    // The compilator sees that return value will be used as starting value of new object
+    // and avoids unnecessery copies.
+
+    Buffer b5 =  RVO_test2(); // in this case RVO will not work
+
+
     return 0;
 }
+/*
+RULE OF 3:
+    If any of: destructor, copy constructor, operator= is needed in out class
+    that means we actualy need all 3 of them.
+
+RULE OF 0:
+    Create classes that so you wont need destructors, copy constructors and =operators.
+    Simply use vectors and unique or shared pointers.
+
+RULE OF 5:
+    ... ???
+*/
