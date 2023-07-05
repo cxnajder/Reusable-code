b4615699e0e8035ad552d7e8d7631d0c860cfaec
cxnajder <cxnajder@github.com>
2023-07-05 20:53:36 +0200

    strategy injection

--- cpp/DesignPatterns/Strategy.cpp
+++ cpp/DesignPatterns/Strategy.cpp
@@ -8,6 +8,19 @@ class RouteStrategy;
 class Map
 {
     std::unique_ptr<RouteStrategy> routeFinder;
+
+public:
+    /*
+     * there are 2 popular ways to inject strategy (set the RouteStrategy object)
+     * 1)   Injection by constructor
+     * 2)   Injection by setter
+     */
+    explicit Map(std::unique_ptr<RouteStrategy> &&routeFinder_ = {}) : routeFinder(std::move(routeFinder_)) {}
+
+    void routeSetter(std::unique_ptr<RouteStrategy> &&routeFinder_)
+    {
+        routeFinder = std::move(routeFinder_);
+    }
 };
 
 
