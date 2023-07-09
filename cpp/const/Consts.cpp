81d48a89918b9c515f7eba6a70f3c55715ccb168
cxnajder <cxnajder@github.com>
2023-07-09 13:15:59 +0200

    const methods manipulations

--- cpp/const/Consts.cpp
+++ cpp/const/Consts.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
 
 class ConstTester {
+    std::string name;
+    mutable int getNameCounter = 0;
+    int getCounterCounter = 0;
 public:
+    
+    ConstTester() { name = "unnamed"; }
 
     void refMethod (int & a) {std::cout << "Ref method : " << a << '\n'; }
     void refMethod (const int & a) {std::cout << "const Ref method : " << a << '\n'; }
 
     void constTest () {std::cout << "Const test : not const\n"; }
     void constTest () const {std::cout << "Const test : const\n"; }
+
+    const std::string & getName() const 
+    {
+        ++getNameCounter; 
+        // becouse getNameCounter parameter is mutable this operation will be allowed even in const method
+        return name; 
+    } 
+
+    int getCounter() const 
+    {
+        (const_cast <ConstTester*> (this))->getCounterCounter += 1; 
+        // even though the method is const, const_cast allows to cast away the constness
+        return getNameCounter; 
+    }
     
+    int getCounterCounterVal() const { return getCounterCounter; }
 };
 
 int main(int argc, char const *argv[])
@@ -52,6 +72,18 @@ int main(int argc, char const *argv[])
 
     const ConstTester t2;
     t2.constTest();
+
+    for(int i = 0; i < 5; ++i)
+        std::cout << t1.getName() << " <-- t1 getName attem " << t1.getCounter() <<'\n';
+
+    std::cout << "GetCounter fun called : " << t1.getCounterCounterVal() <<" times\n";
+    std::cout << "--------------\n";
+
+    for(int i = 0; i < 5; ++i)
+        std::cout << t2.getName() << " <-- t1 getName attem " << t2.getCounter() <<'\n';
+
+    std::cout << "GetCounter fun called : " << t2.getCounterCounterVal() <<" times\n";
+
 
     return 0;
 }
