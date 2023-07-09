0ba5368311114d3e97773972c98622a479361eed
cxnajder <cxnajder@github.com>
2023-07-09 10:25:49 +0200

    const pointers example

--- cpp/const/Consts.cpp
+++ cpp/const/Consts.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
-class Test {
+class ConstTester {
 public:
 
     void refMethod (int & a) {std::cout << "Ref method : " << a << '\n'; }
-    void refMethod (const int & a) {std::cout << "Ref method:" << a << '\n'; }
+    void refMethod (const int & a) {std::cout << "const Ref method : " << a << '\n'; }
 
     void constTest () {std::cout << "Const test : not const\n"; }
     void constTest () const {std::cout << "Const test : const\n"; }
@@ -13,7 +13,33 @@ public:
 
 int main(int argc, char const *argv[])
 {
-    Test t1;
+
+    const int * ptr1 = new int;    // data is const, but ptr not
+    int const * ptr2 = new int;    // the same
+    //*ptr1 = 11; // <-- error: assignment of read-only location ‘* cptr1’
+    //*ptr2 = 13; // same error -//-
+    ++ptr1;
+    --ptr1; // but its allowe to increment and decrement this pointer
+
+    ++ptr2;
+    --ptr2;
+    
+    delete ptr1;     
+    delete ptr2; 
+
+    int * const ptr3 = new int; // now ptr ins const but the value not;
+    *ptr3 = 17;
+    //++ptr3; // <-- error: increment of read-only variable ‘ptr3’
+    //--ptr3;
+    delete ptr3;
+
+    const int * const ptr4 = new int; // const data and const ptr;
+    //*ptr4 = 4; // <-- error: assignment of read-only location ‘*(const int*)ptr4’
+    //++ptr4; // error
+    //--ptr4; // error
+    delete ptr4;
+
+    ConstTester t1;
 
     int x = 20;
     t1.refMethod(x);    //20
@@ -24,7 +50,7 @@ int main(int argc, char const *argv[])
 
     t1.constTest();
 
-    const Test t2;
+    const ConstTester t2;
     t2.constTest();
 
     return 0;
